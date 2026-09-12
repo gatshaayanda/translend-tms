1d2a665d09c184ecb0b400f4890a34425b75cc50
gatshaayanda <93461785+gatshaayanda@users.noreply.github.com>
2026-09-12 16:56:01 +0200

    feat(translend): add v19 trial balance surface

---
+++ src/app/(app)/[orgId]/trial-balance/page.tsx
@@ -0,0 +1,5 @@
+import FinancialAndControlViews from "@/components/v19/FinancialAndControlViews";
+
+export default function TrialBalancePage() {
+  return <FinancialAndControlViews kind="trial" />;
+}
