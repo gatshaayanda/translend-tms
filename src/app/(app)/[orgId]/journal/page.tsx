06f89d8dcc262f761898fe94b7248e5b07a9c92d
gatshaayanda <93461785+gatshaayanda@users.noreply.github.com>
2026-09-12 16:55:48 +0200

    feat(translend): add v19 journal surface

---
+++ src/app/(app)/[orgId]/journal/page.tsx
@@ -0,0 +1,5 @@
+import FinancialAndControlViews from "@/components/v19/FinancialAndControlViews";
+
+export default function JournalPage() {
+  return <FinancialAndControlViews kind="journal" />;
+}
