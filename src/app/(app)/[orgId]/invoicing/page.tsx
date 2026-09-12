ee48d367cf23db90183ca8db777699c039d6967c
gatshaayanda <93461785+gatshaayanda@users.noreply.github.com>
2026-09-12 16:55:38 +0200

    feat(translend): add v19 invoicing surface

---
+++ src/app/(app)/[orgId]/invoicing/page.tsx
@@ -0,0 +1,5 @@
+import FinancialAndControlViews from "@/components/v19/FinancialAndControlViews";
+
+export default function InvoicingPage() {
+  return <FinancialAndControlViews kind="invoicing" />;
+}
