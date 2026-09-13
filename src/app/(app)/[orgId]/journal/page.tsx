7f3c8bc09f324337306efc60e000311c24d3f7a9
gatshaayanda <93461785+gatshaayanda@users.noreply.github.com>
2026-09-13 12:30:51 +0200

    feat: surface accounting controls in journal

--- src/app/(app)/[orgId]/journal/page.tsx
+++ src/app/(app)/[orgId]/journal/page.tsx
@@ -1,5 +1,6 @@
 import FinancialAndControlViews from "@/components/v19/FinancialAndControlViews";
+import AccountingControls from "@/components/v19/AccountingControls";
 
 export default function JournalPage() {
-  return <FinancialAndControlViews kind="journal" />;
+  return <div className="space-y-6"><FinancialAndControlViews kind="journal" /><AccountingControls /></div>;
 }
