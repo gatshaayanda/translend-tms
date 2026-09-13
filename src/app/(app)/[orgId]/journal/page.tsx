4c544f56d9dd2b832182708b8a8e47bf636c63fa
gatshaayanda <93461785+gatshaayanda@users.noreply.github.com>
2026-09-13 13:07:46 +0200

    feat: surface complete accounting controls

--- src/app/(app)/[orgId]/journal/page.tsx
+++ src/app/(app)/[orgId]/journal/page.tsx
@@ -1,6 +1,7 @@
 import FinancialAndControlViews from "@/components/v19/FinancialAndControlViews";
 import AccountingControls from "@/components/v19/AccountingControls";
+import AccountingMaturity from "@/components/v19/AccountingMaturity";
 
 export default function JournalPage() {
-  return <div className="space-y-6"><FinancialAndControlViews kind="journal" /><AccountingControls /></div>;
+  return <div className="space-y-6"><FinancialAndControlViews kind="journal" /><AccountingControls /><AccountingMaturity /></div>;
 }
